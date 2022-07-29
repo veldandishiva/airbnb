6fc451997230c89ba958af8b3eb95b07ab078b41
veldandishiva <84781157+veldandishiva@users.noreply.github.com>
2022-07-29 20:36:56 +0530

    Add files via upload

---
+++ tool.js
@@ -0,0 +1,10 @@
+import Navbar from "./navbar.js"
+
+
+document.getElementById("sign-btn").addEventListener('click',
+function(){
+  document.querySelector(".sign-up").style.display="flex";
+
+})
+
+
